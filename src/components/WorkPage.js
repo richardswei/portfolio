854c2443d7e003b025421f80867bd445aa7be837
Richard <wei.richard.s@gmail.com>
2020-01-14 23:42:39 -0800

    add first work card

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {Card, CardDeck} from 'react-bootstrap'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class WorkPage extends Component {
     componentDidMount() {
@@ -8,10 +8,10 @@ class WorkPage extends Component {
     render() {
         return (
           <CardDeck>
-            <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
+            <Card as='a' target="_blank" href="https://chinese-urban-dictionary.herokuapp.com">
+              <Card.Img variant="top" src="./cud_screenshot.JPG" />
               <Card.Body>
-                <Card.Title>Card title</Card.Title>
+                <Card.Title>Chang's Slang - The Chinese Urban Dictionary</Card.Title>
                 <Card.Text>
                   This is a wider card with supporting text below as a natural lead-in to
                   additional content. This content is a little bit longer.
